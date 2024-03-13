6a2de2a95d32ece94f9d5b231c20cfb8e4933dc9
Danial <danialhakim028@gmail.com>
2024-03-13 14:07:33 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TripController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DriverController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
 
-Route::get('/user', function(Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => 'auth:sanctum'], function (){
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/post', [DriverController::class, 'update']);
+
+    Route::post('/trip', [TripController::class, 'store']);
+    Route::get('/trip/{trip}',[TripController::class, 'show']);
+    Route::post('/trip/{trip}/accept', [TripController::class, 'accept']);
+    Route::post('/trip/{trip}/start', [TripController::class, 'start']);
+    Route::post('/trip/{trip}/end', [TripController::class, 'end']);
+    Route::post('/trip/{trip}/location', [TripController::class, 'location']);
+
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
+});
+
